f08003365c26ac224b055e0dc96206634c5ec899
mdazadabdullah <jobs4angular@gmail.com>
2025-12-24 23:34:49 +0530

    implemented login code.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,52 @@
 const express = require("express")
 const connectDB = require("./config/database")
 const User = require('./models/user')
+const { validateSignUpData } = require('./utils/validation')
+const bcrypt = require("bcrypt");
+
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    const user = new User(req.body);
     try {
+        // validation of data
+        validateSignUpData(req);
+        const { firstName, lastName, password, emailId } = req.body;
+        // Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
         await user.save();
         res.send("User added successfully");
     } catch (err) {
         res.status(400).send("Error saving the user" + err.message);
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId: emailId });
+        if(!user) {
+            throw new Error("Invalid credentials");
+        }
+        // const isPasswordValid = await bcrypt.compare(passowrd, user.passowrd);
+        const isPasswordValid = await bcrypt.compare(String(password), String(user.password));
+        console.log("isPasswordValid", isPasswordValid);
+        if(isPasswordValid) {
+            res.send("Login Successful !!! ");
+        }else {
+            throw new Error("Invalid credentials");
+        }
+
+    } catch (err) {
+        res.status(400).send("Error:" + err.message);
     }
 })
 
@@ -39,13 +74,15 @@ app.patch('/user', async(req, res) => {
     const userId = req.body.userId;
     const data = req.body;
     try {
-        await User.findByIdAndUpdate({_id: userId }, data);
+        await User.findByIdAndUpdate({_id: userId }, data, {
+            returnDocument: "after",
+            runValidators: true
+        });
         res.send("User updated successfully");
     }catch (err) {
         res.status(400).send("Something went wrong");
     }
 })
-
 
 app.get('/feed', async(req, res) => {
     try {
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,54 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 12,
     },
     lastName: {
-        type: String
+        type: String,
+        minLength: 4,
+        maxLength: 12,
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
-    }
-});
+        type: String,
+        validate(value) {
+            if(!["male", "female", "others"].includes(value) ) {
+                throw new Error("Gender data is not a valid");
+            }
+        },
+    },
+    photoUrl: {
+        type: String,
+        default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+        type: String,
+        default: "This is the defaukt about of the user",
+    },
+    skills: {
+        type: [String]
+    },
+},
+{
+    timestamps: true,
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
